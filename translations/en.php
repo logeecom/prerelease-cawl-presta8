f043d50df5d1d7b13193c77bf9876a212e4cd285
Worldline-plugins-1 <dev+WorldlinePlugins@logeecom.com>
2025-09-17 14:14:13 +0000

    Push plugin cawl version 1.1.0

--- translations/en.php
+++ translations/en.php
@@ -4,8 +4,17 @@ namespace {
     global $_MODULE;
     $_MODULE = array();
     $_MODULE['<{cawlop}prestashop>cawlop_a9928cf14ad15878c88657a133832f4b'] = 'This module offers a 1-click integration to start accepting payments and grow your revenues by offering your customers with global and regional payment methods to sell across Europe.';
+    $_MODULE['<{cawlop}prestashop>redirectmodulefrontcontroller_187370742861bbe6eb6468a587e055d4'] = 'An error occurred while fetching the payment status.';
+    $_MODULE['<{cawlop}prestashop>customeraccountextensioncontroller_0d02a983cd676aa3b4d555cb6918dff5'] = 'Could not delete stored card.';
+    $_MODULE['<{cawlop}prestashop>customeraccountextensioncontroller_70f6d9f3075b1ecd1768624894a94472'] = 'Card deleted successfully.';
+    $_MODULE['<{cawlop}prestashop>paymentmodulefrontcontroller_abcd10a3b2f38f78042c8fd5f5e30ca4'] = 'An error occurred while processing the payment.';
+    $_MODULE['<{cawlop}prestashop>transactioncontroller_96ffad83b6b1b37f4ea7b290958c98d3'] = 'You do not have permission to capture funds.';
+    $_MODULE['<{cawlop}prestashop>transactioncontroller_8ff2a8d9d1c17495a20bbce1770dbc0d'] = 'You do not have permission to refund funds.';
+    $_MODULE['<{cawlop}prestashop>transactioncontroller_316a88b88dbc5fbf5efd3bc6030865d4'] = 'You do not have permission to cancel transactions.';
+    $_MODULE['<{cawlop}prestashop>transactioncontroller_c9d760a3e5fa9bbd09ebe00b0dab8872'] = 'Unexpected error occurred during payment link creation.';
     $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_b44ba42b352146bf2fe51a2c6264cb0c'] = 'Please select the specific shop to configure.';
     $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_e89daa9df8a55b1329488a95eb11045b'] = 'Please enable the module.';
+    $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_8cff07b6e3f3463a12fe640f7abae944'] = 'Payment Reference';
     $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_35eedfe47e1e044854d5f05bdaaca80c'] = 'An error occurred while processing your request. Please try again.';
     $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_97b2ab491f2b6cb5af9a361aa94e6239'] = 'Do you confirm the refund of the funds?';
     $_MODULE['<{cawlop}prestashop>onlinepaymentsmodule_ea70ced75c5a05889a136ee4601a4cdc'] = 'Do you confirm the capture of the transaction?';
@@ -20,14 +29,11 @@ namespace {
     $_MODULE['<{cawlop}prestashop>paymentoptionsservice_6294b8b61ea3540661e970803ddc382c'] = 'Pay with my previously saved card %s';
     $_MODULE['<{cawlop}prestashop>cancelservice_f23864b36b1b7b82e983d15f10eb44df'] = 'Unexpected error occurred during refund.';
     $_MODULE['<{cawlop}prestashop>cancelservice_5e7825b74250aebcde06c1e77ebc9709'] = 'Cancellation request successfully sent.';
-    $_MODULE['<{cawlop}prestashop>captureservice_f23864b36b1b7b82e983d15f10eb44df'] = 'Unexpected error occurred during refund.';
+    $_MODULE['<{cawlop}prestashop>cancelservice_9d18bbe4ed49510e3037c99fafb721c5'] = 'Unexpected error occurred during cancellation.';
+    $_MODULE['<{cawlop}prestashop>cancelservice_a502d4c089bd32e1a01e12815d8840ef'] = 'Cancel creation failed on %s!';
+    $_MODULE['<{cawlop}prestashop>captureservice_d80f159d02805191dacb91df4c7535c9'] = 'Unexpected error occurred during capture.';
+    $_MODULE['<{cawlop}prestashop>captureservice_1bbfc8883015b76e95c23f9b1555e2f0'] = 'Capture creation failed on %s!';
     $_MODULE['<{cawlop}prestashop>refundservice_f23864b36b1b7b82e983d15f10eb44df'] = 'Unexpected error occurred during refund.';
+    $_MODULE['<{cawlop}prestashop>refundservice_a37b2918e2ddb71efeda2824e0e41aed'] = 'Refund creation failed on %s!';
     $_MODULE['<{cawlop}prestashop>refundservice_48373583c611d226c86ee1ad21912c23'] = 'Unexpected error occurred during refund';
-    $_MODULE['<{cawlop}prestashop>customeraccountextensioncontroller_0d02a983cd676aa3b4d555cb6918dff5'] = 'Could not delete stored card.';
-    $_MODULE['<{cawlop}prestashop>customeraccountextensioncontroller_70f6d9f3075b1ecd1768624894a94472'] = 'Card deleted successfully.';
-    $_MODULE['<{cawlop}prestashop>redirectmodulefrontcontroller_187370742861bbe6eb6468a587e055d4'] = 'An error occurred while fetching the payment status.';
-    $_MODULE['<{cawlop}prestashop>paymentmodulefrontcontroller_abcd10a3b2f38f78042c8fd5f5e30ca4'] = 'An error occurred while processing the payment.';
-    $_MODULE['<{cawlop}prestashop>transactioncontroller_96ffad83b6b1b37f4ea7b290958c98d3'] = 'You do not have permission to capture funds.';
-    $_MODULE['<{cawlop}prestashop>transactioncontroller_8ff2a8d9d1c17495a20bbce1770dbc0d'] = 'You do not have permission to refund funds.';
-    $_MODULE['<{cawlop}prestashop>transactioncontroller_316a88b88dbc5fbf5efd3bc6030865d4'] = 'You do not have permission to cancel transactions.';
 }
